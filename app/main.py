97bca0e7ba3525b6cf48b452355cac2c7e923a36
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-11-13 14:54:27 -0300

    feat: add cam icons

--- app/main.py
+++ app/main.py
@@ -5,8 +5,21 @@ import pandas as pd
 import pydeck as pdk
 import streamlit as st
 
+ICON_URL = "https://cdn-icons-png.flaticon.com/512/2060/2060364.png"
+icon_data = {
+    # Icon from Wikimedia, used the Creative Commons Attribution-Share Alike 3.0
+    # Unported, 2.5 Generic, 2.0 Generic and 1.0 Generic licenses
+    "url": ICON_URL,
+    "width": 242,
+    "height": 242,
+    "anchorY": 242,
+}
+
 tmp_data_path = Path(__file__).parent.parent / "data" / "cameras_h3_1.csv"
 chart_data = pd.read_csv(tmp_data_path)
+chart_data["icon_data"] = None
+for i in chart_data.index:
+    chart_data["icon_data"][i] = icon_data
 
 st.pydeck_chart(
     pdk.Deck(
@@ -19,11 +32,13 @@ st.pydeck_chart(
         ),
         layers=[
             pdk.Layer(
-                "ScatterplotLayer",
+                "IconLayer",
                 data=chart_data,
+                get_icon="icon_data",
                 get_position="[longitude, latitude]",
-                get_color="[200, 30, 0, 160]",
-                get_radius=200,
+                get_size=4,
+                size_scale=5,
+                pickable=True,
             ),
         ],
         tooltip={
